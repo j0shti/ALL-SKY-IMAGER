f3abf752a96cdc01ac9f2a270a23a85e42687be8
josh <joshuaturi391@gmail.com>
2025-07-14 12:41:08 -0400

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -1,5 +1,10 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install python-is-python3
+
 sudo mkdir /logs
 mkdir -p /ALL-SKY-IMAGER/data
 sudo apt-get install bzip2
 cd ALL-SKY-IMAGER/zwo
 tar -xvf ASI_linux_mac_SDK_V1.38.tar.bz2
+chmod a+x ALL-SKY-IMAGER/clockmaker
+sudo ./ALL-SKY-IMAGER/clockmaker --config
