a1d944c72b4096deb69b8512976f2970526c00ca
josh <joshuaturi391@gmail.com>
2025-07-21 10:21:41 -0400

    ytes

--- configuration2.sh
+++ configuration2.sh
@@ -1,4 +1,8 @@
-./ALL-SKY-IMAGER/clockmaker --build
+user=$(whoami)
+
+cd /home/$user/ALL-SKY-IMAGER/
+./clockmaker --build
 cd /home/$user/ALL-SKY-IMAGER/gps_service/
 chmod a+x run_gps.sh
+chmod a+x startcam.sh
 
--- gps_service/run_gps.sh
+++ gps_service/run_gps.sh
@@ -6,8 +6,8 @@ MAX_SYNC_TIME=1200 # Time limit for checking the system time synchronization wit
 user="$(whoami)"
 
 LOG_PATH='/logs'
-ENV_PATH='/home/'$user'/ALL-SKY-IMAGER/zwo'
-PROGRAM=$ENV_PATH'autostartcam.sh'
+ENV_PATH='/home/'$user'/ALL-SKY-IMAGER/gps_service'
+PROGRAM=$ENV_PATH'startcam.sh'
 
 ST="$(date +%s)"
 
