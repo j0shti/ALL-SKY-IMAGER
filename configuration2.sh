7e861ce50e149a6d20e1b0ec818217f4f0a203de
josh <joshuaturi391@gmail.com>
2025-07-21 11:26:32 -0400

    hello

--- configuration2.sh
+++ configuration2.sh
@@ -1,7 +1,11 @@
 user=$(whoami)
 
+# finish NTPsec setup
+chmod a+x ALL-SKY-IMAGER/clockmaker
 cd /home/$user/ALL-SKY-IMAGER/
 ./clockmaker --build
+
+# enable gps service finally
 cd /home/$user/ALL-SKY-IMAGER/gps_service/
 chmod a+x run_gps.sh
 chmod a+x startcam.sh
