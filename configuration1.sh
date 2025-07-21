b6c97b50335f21df3373a33df53f999cb857977a
josh <joshuaturi391@gmail.com>
2025-07-21 09:36:51 -0400

    asiud

--- configuration1.sh
+++ configuration1.sh
@@ -2,10 +2,12 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install python-is-python3 -y
 
 # camera script setup
+user = $(whoami)
+
 sudo mkdir /logs
-mkdir -p /ALL-SKY-IMAGER/data
+mkdir -p /home/$user/ALL-SKY-IMAGER/data
 sudo apt-get install bzip2 -y
-cd ALL-SKY-IMAGER/zwo
+cd /home/$user/ALL-SKY-IMAGER/zwo
 tar -xvf ASI_linux_mac_SDK_V1.38.tar.bz2
 
 # NTPsec setup
