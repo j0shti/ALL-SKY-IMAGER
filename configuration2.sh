c7f06aede222d11dde0e9dffa257a13ebc0b17c6
josh <joshuaturi391@gmail.com>
2025-07-21 13:24:20 -0400

    Update configuration2.sh

--- configuration2.sh
+++ configuration2.sh
@@ -13,14 +13,12 @@ sudo chmod a+x run_gps.sh
 sudo chmod a+x startcam.sh
 
 # create systemd service ot autostart gps service
-echo "switch to root"
 cd ../
 sudo mv gps_service /
 sudo mv autostart.service /etc/systemd/system
 cd /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable --now autostart.service
-exit
 
 # restart system
 echo "configuration complete--rebooting"
