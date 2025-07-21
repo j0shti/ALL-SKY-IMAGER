258566eb8c54830bebc52c23d84a9398d7bc8da9
josh <joshuaturi391@gmail.com>
2025-07-21 11:40:05 -0400

    Update configuration2.sh

--- configuration2.sh
+++ configuration2.sh
@@ -5,8 +5,12 @@ chmod a+x /home/$user/ALL-SKY-IMAGER/clockmaker
 cd /home/$user/ALL-SKY-IMAGER/
 ./clockmaker --build
 
-# enable gps service finally
+# enable gps service
 cd /home/$user/ALL-SKY-IMAGER/gps_service/
 chmod a+x run_gps.sh
 chmod a+x startcam.sh
+mv startcam.sh /
 
+# create systemd service ot autostart gps service
+sudo -s
+cd /etc/systemd/system
