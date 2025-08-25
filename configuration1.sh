def0f6022e20b05428b8ffd724179355f0b8e463
josh <joshuaturi391@gmail.com>
2025-08-24 23:49:23 -0400

    Delete configuration1.sh

--- configuration1.sh
+++
@@ -1,8 +0,0 @@
-tar -xvf ASI_linux_mac_SDK_V1.38.tar.bz2
-sudo apt install python3-ephem -y
-sudo apt install python3-smbus -y
-sudo apt install python3-pillow -y
-
-# NTPsec setup
-sudo chmod a+x ALL-SKY-IMAGER/clockmaker
-sudo ./ALL-SKY-IMAGER/clockmaker --config
